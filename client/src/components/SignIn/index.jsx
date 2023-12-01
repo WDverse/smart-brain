9351f80c0d8bb685b4327f57b9676ca2725c9528
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2023-11-30 22:37:52 -0500

    connected signIn route to frontend

--- client/src/components/SignIn/index.jsx
+++ client/src/components/SignIn/index.jsx
@@ -1,6 +1,33 @@
-import React from "react";
+import { React, useState } from "react";
 
 const SignIn = ({ onRouteChange }) => {
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+
+  const onEmailChange = (event) => {
+    setUserEmail(event.target.value);
+  };
+  const onPasswordChange = (event) => {
+    setUserPassword(event.target.value);
+  };
+
+  const onSubmitSignin = () => {
+    fetch("http://localhost:3000/signin", {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        email: userEmail,
+        password: userPassword,
+      }),
+    }).then((response)=>{
+      return response.json()
+    }).then((data)=> {
+      if (data === "Success"){
+        onRouteChange("home");
+      }
+    })
+  };
+
   return (
     <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
       <main className="pa4 black-80">
@@ -16,6 +43,7 @@ const SignIn = ({ onRouteChange }) => {
                 type="email"
                 name="email-address"
                 id="email-address"
+                onChange={onEmailChange}
               />
             </div>
             <div className="mv3">
@@ -27,6 +55,7 @@ const SignIn = ({ onRouteChange }) => {
                 type="password"
                 name="password"
                 id="password"
+                onChange={onPasswordChange}
               />
             </div>
           </fieldset>
@@ -35,11 +64,14 @@ const SignIn = ({ onRouteChange }) => {
               className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
               type="submit"
               value="Sign in"
-              onClick={() => onRouteChange("home")}
+              onClick={onSubmitSignin}
             />
           </div>
           <div className="lh-copy mt3">
-            <p  onClick={()=> onRouteChange("register")} className="f6 link dim black db pointer">
+            <p
+              onClick={() => onRouteChange("register")}
+              className="f6 link dim black db pointer"
+            >
               Register
             </p>
           </div>
