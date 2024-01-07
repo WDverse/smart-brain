fc2bf70b30ef88dfb474cc3c5e35b79e698f4bba
Emmanuel Appiagyei <e_appiagyei@outlook.com>
2024-01-07 01:44:14 -0500

    removed entries from display

--- src/components/Entries/index.jsx
+++ src/components/Entries/index.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 
-const Rank = ({userName, userEntries}) => {
+const Rank = ({userName}) => {
   return (
     <div>
-      <div className="white f3">{`${userName}, your current entry count is...`}</div>
-      <div className="white f1">{`${userEntries}`}</div>
+      <div className="white f3">{`Welcome ${userName}!`}</div>
     </div>
   );
 };
